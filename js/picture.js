19949e16e95fe0520195754ffd617b5e4c713d61
KseniaBezverhaia <ks.bezverhy@icloud.com>
2024-11-18 15:42:40 +0500

    Испаравление ошибок

--- js/picture.js
+++ js/picture.js
@@ -11,7 +11,7 @@ similarPictures.forEach((picture) => {
   elementPictures.querySelector('.picture__img').alt = picture.description;
   elementPictures.querySelector('.picture__likes').textContent = picture.likes;
   elementPictures.querySelector('.picture__comments').textContent = picture.comments.length;
-  elementPictures.setAttribute('data-myArray', JSON.stringify(picture.comments));
+  elementPictures.querySelector('.picture__comments').dataset.array = picture.comments;
   similarListFragment.appendChild(elementPictures);
 });
 similarListElement.appendChild(similarListFragment);
