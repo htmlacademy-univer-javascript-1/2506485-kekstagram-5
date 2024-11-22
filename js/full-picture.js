b7fb0d6d8f2d02b946b11d310466a2b5c4def3eb
KseniaBezverhaia <ks.bezverhy@icloud.com>
2024-11-22 12:31:27 +0500

    вывод списка комментариев

--- js/full-picture.js
+++ js/full-picture.js
@@ -8,23 +8,39 @@ const fullPicturesImg = userFullPhoto.querySelector('.big-picture__img img');
 const fullPicturesLikes = userFullPhoto.querySelector('.likes-count');
 const fullPicturesCommentsCount = userFullPhoto.querySelector('.comments-count');
 const fullPicturesDescription = userFullPhoto.querySelector('.social__caption');
+const addCommentsButton = document.querySelector('.social__comments-loader');
+const commentsCount = userFullPhoto.querySelector('.social__comment-count');
 
-const addComments = (array) => {
-  for (let i = 0; i < array.length; i++){
-    const element = document.createElement('li');
-    element.classList.add('social__comment');
-    const image = document.createElement('img');
-    image.classList.add('social__picture');
-    image.src = array[i].avatar;
-    image.alt = array[i].name;
-    image.width = 35;
-    image.height = 35;
-    const text = document.createElement('p');
-    text.classList.add('social__text');
-    text.textContent = array[i].message;
-    element.appendChild(image);
-    element.appendChild(text);
-    commentContainer.appendChild(element);
+const renderingComments = (array, i, k) =>{
+  const element = document.createElement('li');
+  element.classList.add('social__comment');
+  const image = document.createElement('img');
+  image.classList.add('social__picture');
+  image.src = array[k * 5 + i].avatar;
+  image.alt = array[k * 5 + i].name;
+  image.width = 35;
+  image.height = 35;
+  const text = document.createElement('p');
+  text.classList.add('social__text');
+  text.textContent = array[k * 5 + i].message;
+  element.appendChild(image);
+  element.appendChild(text);
+  commentContainer.appendChild(element);
+};
+
+const addComments = (array, k) => {
+  const length = array.length;
+  for (let i = 0; i < 5; i++){
+    if (length > (k * 5 + i)){
+      renderingComments(array, i, k);
+      let count;
+      if ((k + 1) * 5 < length){
+        count = (k + 1) * 5;
+      }else {
+        count = length;
+      }
+      commentsCount.textContent = `${count} из ${length} комментариев`;
+    }
   }
 };
 
@@ -40,7 +56,12 @@ similarListElement.addEventListener('click', (evt) => {
   fullPicturesDescription.textContent = target.querySelector('.picture__img').alt;
   const array = JSON.parse(target.querySelector('.picture__comments').dataset.array);
   commentContainer.innerHTML = '';
-  addComments(array);
+  let k = 0;
+  addComments(array, k);
+  addCommentsButton.addEventListener('click', () => {
+    k += 1;
+    addComments(array, k);
+  });
 });
 
 document.addEventListener('keydown', (evt) => {
